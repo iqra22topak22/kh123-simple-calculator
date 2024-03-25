6473c329f8abe464baa1cc6bfd0f1938278c41ed
iqra22topak22 <161644859+iqra22topak22@users.noreply.github.com>
2024-03-25 15:41:32 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answer = await inquirer.prompt([
+  { message: "enrt your first num ", type: "number", name: "firstNumber" },
+  { message: "enrt your first num ", type: "number", name: "secondNumber" },
+  {
+    message: "select ane of operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISSION"],
+  },
+]);
+  // CONDITIONAL STATEMENT
+  if (answer. operator ==="ADDITION"){
+    console.log(answer.firstNumber + answer.secondNumber);
+
+  }else if(answer. operator ==="SUBTRACTION"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}else if(answer. operator ==="MULTIPLICATION"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}else if(answer. operator ==="DIVISSION"){
+    console.log(answer.firstNumber / answer.secondNumber);
+
+  }
+  else{"please select correct operator"};
+
+
+
